5c0ef6694b0f1966f97af5670b63cff8ff113a5f
Tom <tttrinter@gmail.com>
2015-01-23 07:25:39 -0500

    Updated the comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Coursera - R Programming Course - Programming Assignment 2 
 
 ## The makeCacheMatrix function creates a special "vector", which is
 # really a list containing a function to
@@ -27,7 +26,6 @@ makeCacheMatrix <- function(x = matrix()) {
          getInv = getInv)
   }
 
-## Write a short comment describing this function
 # The following function calculates the inverse of the matrix in the special "vector"
 # created with the above function. However, it first checks to see if the
 # inverse has already been calculated. If so, it `get`s the inverse from the
